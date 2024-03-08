67c386acbe37f3f4dfe6bee9c3296e3c67754a64
Ha Tran <nganha1897@gmail.com>
2024-03-07 23:48:38 -0500

    Time: 886 ms (41.32%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,8 +1,12 @@
 /* Write your PL/SQL query statement below */
-SELECT a1.machine_id, ROUND((SUM(a2.timestamp) - SUM(a1.timestamp)) / COUNT(a1.machine_id), 3) processing_time
-FROM Activity a1, Activity a2
-WHERE a1.machine_id = a2.machine_id
-AND a1.process_id = a2.process_id
-AND a1.activity_type = 'start'
-AND a2.activity_type = 'end'
-GROUP BY a1.machine_id;
+SELECT 
+    machine_id, 
+    ROUND(
+        SUM(
+            CASE WHEN activity_type = 'start' THEN timestamp * -1
+            ELSE timestamp 
+            END
+        ) / (COUNT(DISTINCT process_id))
+    , 3) AS processing_time
+FROM Activity
+GROUP BY machine_id;
