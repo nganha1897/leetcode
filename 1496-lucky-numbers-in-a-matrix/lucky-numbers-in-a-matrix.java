4fda996242700d49d6b6274d0f1790747a340607
Ha Tran <nganha1897@gmail.com>
2024-07-19 20:07:48 -0400

    Time: 2 ms (93.89%) | Memory: 45.1 MB (51.44%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,29 +1,30 @@
 class Solution {
-    public List<Integer> luckyNumbers(int[][] matrix) {
-        int N = matrix.length, M = matrix[0].length;
-
-        int rMinMax = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            int rMin = Integer.MAX_VALUE;
-            for (int j = 0; j < M; j++) {
-                rMin = Math.min(rMin, matrix[i][j]);
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        List<Integer> ans = new ArrayList<>();
+        for (int r=0; r<m; r++) {
+            int min = matrix[r][0];
+            int minCol = 0;
+            for (int c=0; c<n; c++) {
+                if (matrix[r][c] < min) {
+                    min = matrix[r][c];
+                    minCol = c; 
+                }
             }
-            rMinMax = Math.max(rMinMax, rMin);
-        }
 
-        int cMaxMin = Integer.MAX_VALUE;
-        for (int i = 0; i < M; i++) {
-            int cMax = Integer.MIN_VALUE;
-            for (int j = 0; j < N; j++) {
-                cMax = Math.max(cMax, matrix[j][i]);
+            int max = matrix[0][minCol];
+            int maxRow = 0;
+            for (int curR=0; curR<m; curR++) {
+                if (matrix[curR][minCol] > max) {
+                    max = matrix[curR][minCol];
+                    maxRow = curR;
+                }
             }
-            cMaxMin = Math.min(cMaxMin, cMax);
+
+            if (r == maxRow)
+                ans.add(matrix[r][minCol]);
         }
 
-        if (rMinMax == cMaxMin) {
-            return new ArrayList<>(Arrays.asList(rMinMax));
-        }
-
-        return new ArrayList<>();
+        return ans;
     }
 }
