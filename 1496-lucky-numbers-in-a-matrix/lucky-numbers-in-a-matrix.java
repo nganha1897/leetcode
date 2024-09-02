047a3fb1162fefa0fc632bb68b8aebf6d36860ff
Ha Tran <nganha1897@gmail.com>
2024-09-02 18:52:44 -0400

    Time: 3 ms (61.45%) | Memory: 44.9 MB (97.60%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,29 +1,58 @@
+// class Solution {
+//     public List<Integer> luckyNumbers(int[][] matrix) {
+//         List<Integer> ans = new ArrayList<>();
+//         int m = matrix.length, n = matrix[0].length;
+
+//         int maxMinRow = 0;
+//         for (int r = 0; r < m; r++) {
+//             int min = matrix[r][0];
+//             for (int c = 0; c < n; c++) {
+//                 min = Math.min(min, matrix[r][c]);
+//             }
+//             maxMinRow = Math.max(maxMinRow, min);
+//         }
+
+//         int minMaxCol = 100001;
+//         for (int c = 0; c < n; c++) {
+//             int max = matrix[0][c];
+//             for (int r = 0; r < m; r++) {
+//                 max = Math.max(max, matrix[r][c]);
+//             }
+//             minMaxCol = Math.min(minMaxCol, max);
+//         }
+
+//         if (maxMinRow == minMaxCol) {
+//             ans.add(maxMinRow);
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
-        List<Integer> ans = new ArrayList<>();
-        int m = matrix.length, n = matrix[0].length;
+        int N = matrix.length, M = matrix[0].length;
 
-        int maxMinRow = 0;
-        for (int r = 0; r < m; r++) {
-            int min = matrix[r][0];
-            for (int c = 0; c < n; c++) {
-                min = Math.min(min, matrix[r][c]);
+        int rMinMax = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            int rMin = Integer.MAX_VALUE;
+            for (int j = 0; j < M; j++) {
+                rMin = Math.min(rMin, matrix[i][j]);
             }
-            maxMinRow = Math.max(maxMinRow, min);
+            rMinMax = Math.max(rMinMax, rMin);
         }
 
-        int minMaxCol = 100001;
-        for (int c = 0; c < n; c++) {
-            int max = matrix[0][c];
-            for (int r = 0; r < m; r++) {
-                max = Math.max(max, matrix[r][c]);
+        int cMaxMin = Integer.MAX_VALUE;
+        for (int i = 0; i < M; i++) {
+            int cMax = Integer.MIN_VALUE;
+            for (int j = 0; j < N; j++) {
+                cMax = Math.max(cMax, matrix[j][i]);
             }
-            minMaxCol = Math.min(minMaxCol, max);
+            cMaxMin = Math.min(cMaxMin, cMax);
         }
 
-        if (maxMinRow == minMaxCol) {
-            ans.add(maxMinRow);
+        if (rMinMax == cMaxMin) {
+            return new ArrayList<>(Arrays.asList(rMinMax));
         }
-        return ans;
+
+        return new ArrayList<>();
     }
 }
