2dcc6f04340f69dc2505b10bf5b5abd311e41448
Ha Tran <nganha1897@gmail.com>
2024-09-02 18:05:02 -0400

    Time: 0 ms (100.00%) | Memory: 41.6 MB (5.44%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n];
+        int[] dp = new int[n];
 
-        for (int i=n-1; i>=0; i--) {
-            dp[i][i] = nums[i];
-            for (int j=i+1; j<n; j++) {
-                dp[i][j] = Math.max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1]);
-            }
+        for (int i=0; i<n; i++) {
+            dp[i] = nums[i];
         }
-        return dp[0][n-1] >= 0;
+        for (int i=n-1; i>=0; i--) {
+            int[] curDp = new int[n];
+            curDp[i] = nums[i];
+            for (int j=i+1; j<n; j++) {
+                curDp[j] = Math.max(nums[i] - dp[j], nums[j] - curDp[j-1]);
+            }
+            dp = curDp;
+        }
+        return dp[n-1] >= 0;
     }
 }
