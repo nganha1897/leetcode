66d790388f4e92ffd5c3ca721f509d4627886d8e
Ha Tran <nganha1897@gmail.com>
2024-09-02 19:53:57 -0400

    Time: 1 ms (97.07%) | Memory: 45.2 MB (64.77%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -3,20 +3,26 @@ class Solution {
         int m = matrix.length, n = matrix[0].length;
         List<Integer> ans = new ArrayList<>();
         for (int r=0; r<m; r++) {
-            int min = matrix[r][0];
-            int minCol = 0;
-            for (int c=0; c<n; c++) {
-                if (matrix[r][c] < min) {
-                    min = matrix[r][c];
-                    minCol = c; 
-                }
-            }
+            int minCol = findMinCInR(matrix,r);
+            int min = matrix[r][minCol];
 
             if (checkIfMaxInC(matrix, minCol, min))
                 ans.add(min);
         }
 
         return ans;
+    }
+
+    private int findMinCInR(int[][] m,int r){
+        int min = 100001,
+            minIndex = 0;
+        for(int c=0;c<m[r].length;c++){
+            if(m[r][c]<min){
+                min = m[r][c];
+                minIndex = c;
+            }
+        }
+        return minIndex;
     }
 
     private boolean checkIfMaxInC(int[][]m, int c, int max){
