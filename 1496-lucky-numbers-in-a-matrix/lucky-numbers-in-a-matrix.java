e5c3aee18d4f0c366201744f20f4a7133b8afae2
Ha Tran <nganha1897@gmail.com>
2024-09-02 19:43:28 -0400

    Time: 2 ms (92.88%) | Memory: 45.1 MB (82.01%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,69 +1,30 @@
-// class Solution {
-//     public List<Integer> luckyNumbers(int[][] matrix) {
-//         List<Integer> ans = new ArrayList<>();
-//         int m = matrix.length, n = matrix[0].length;
-
-//         int maxMinRow = 0;
-//         for (int r = 0; r < m; r++) {
-//             int min = matrix[r][0];
-//             for (int c = 0; c < n; c++) {
-//                 min = Math.min(min, matrix[r][c]);
-//             }
-//             maxMinRow = Math.max(maxMinRow, min);
-//         }
-
-//         int minMaxCol = 100001;
-//         for (int c = 0; c < n; c++) {
-//             int max = matrix[0][c];
-//             for (int r = 0; r < m; r++) {
-//                 max = Math.max(max, matrix[r][c]);
-//             }
-//             minMaxCol = Math.min(minMaxCol, max);
-//         }
-
-//         if (maxMinRow == minMaxCol) {
-//             ans.add(maxMinRow);
-//         }
-//         return ans;
-//     }
-// }
-
 class Solution {
-    public List<Integer> luckyNumbers(int[][] m) {
-        List<Integer> list = new ArrayList<Integer>();
-        int item,
-        minC;
-
-        for(int r=0;r<m.length;r++){
-            minC=findMinCInR(m,r);
-            item = m[r][minC];
-       
-            if(checkIfMaxInC(m,minC,item)){
-                list.add(item);
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        List<Integer> ans = new ArrayList<>();
+        for (int r=0; r<m; r++) {
+            int min = matrix[r][0];
+            int minCol = 0;
+            for (int c=0; c<n; c++) {
+                if (matrix[r][c] < min) {
+                    min = matrix[r][c];
+                    minCol = c; 
+                }
             }
-        }
- 
-        return list;
-    }
 
-    private int findMinCInR(int[][] m,int r){
-        int min = 100001,
-            minIndex = 0;
-        for(int c=0;c<m[r].length;c++){
-            if(m[r][c]<min){
-                min = m[r][c];
-                minIndex = c;
+            int max = matrix[0][minCol];
+            int maxRow = 0;
+            for (int curR=0; curR<m; curR++) {
+                if (matrix[curR][minCol] > max) {
+                    max = matrix[curR][minCol];
+                    maxRow = curR;
+                }
             }
-        }
-        return minIndex;
-    }
 
-    private boolean checkIfMaxInC(int[][]m, int c, int max){
-        for(int r=0;r<m.length;r++){
-            if(m[r][c]>max){
-                return false;
-            }
+            if (r == maxRow)
+                ans.add(matrix[r][minCol]);
         }
-        return true;
+
+        return ans;
     }
 }
