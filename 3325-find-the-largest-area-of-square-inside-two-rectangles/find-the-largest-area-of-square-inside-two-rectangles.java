d5e3d490aa326caa485426929f76707110f6e4d1
Ha Tran <nganha1897@gmail.com>
2024-09-02 05:18:59 -0400

    Time: 99 ms (44.03%) | Memory: 44.9 MB (75.37%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
@@ -3,20 +3,16 @@ class Solution {
         long ans = 0;
         for (int i=0; i<bottomLeft.length-1; i++) {
             for (int j=i+1; j<bottomLeft.length; j++) {
-                int xti = topRight[i][0];
-                int xbi = bottomLeft[i][0];
-                int xtj = topRight[j][0];
-                int xbj = bottomLeft[j][0];
-                long side1 = Math.min(xti, xtj) - Math.max(xbi, xbj);
+                long minX = Math.min(topRight[i][0], topRight[j][0]);
+                long maxX = Math.max(bottomLeft[i][0], bottomLeft[j][0]);
 
-                int yti = topRight[i][1];
-                int ybi = bottomLeft[i][1];
-                int ytj = topRight[j][1];
-                int ybj = bottomLeft[j][1];
-                long side2 = Math.min(yti, ytj) - Math.max(ybi, ybj);
+                long minY = Math.min(topRight[i][1], topRight[j][1]);
+                long maxY = Math.max(bottomLeft[i][1], bottomLeft[j][1]);
 
-                if (side1 > 0 && side2 > 0) {
-                    ans = Math.max(ans, Math.min(side1, side2) * Math.min(side1, side2));
+                long side = Math.min(minX - maxX, minY - maxY);
+
+                if (side > 0) {
+                    ans = Math.max(ans, side * side);
                 }
             }
         }
