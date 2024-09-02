0a5a63e4b8657e6532f94538731da159e2904bf3
Ha Tran <nganha1897@gmail.com>
2024-09-02 17:50:24 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (28.60%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,28 +1,14 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        int n = nums.length, sum = 0;
+        int n = nums.length;
+        int[][] dp = new int[n][n];
 
-        for (int num : nums) {
-            sum += num;
-        }
-        return predict(nums, 0, n - 1, sum, 0, true);
-    }
-
-    private boolean predict(int[] nums, int i, int j, int sum, int score, boolean isTurn) {
-        if (i == j && !isTurn) {
-            return (sum - score) > score;
-        } else if (i == j) {
-            return (score + nums[i]) >= (sum - score - nums[i]);
-        } else {
-            if (isTurn) {
-                boolean opt1 = predict(nums, i + 1, j, sum, score + nums[i], !isTurn);
-                boolean opt2 = predict(nums, i, j - 1, sum, score + nums[j], !isTurn);
-                return (!opt1 || !opt2);
-            } else {
-                boolean opt1 = predict(nums, i + 1, j, sum, score, !isTurn);
-                boolean opt2 = predict(nums, i, j - 1, sum, score, !isTurn);
-                return (!opt1 || !opt2);
+        for (int i=n-1; i>=0; i--) {
+            dp[i][i] = nums[i];
+            for (int j=i+1; j<n; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1]);
             }
         }
+        return dp[0][n-1] >= 0;
     }
 }
