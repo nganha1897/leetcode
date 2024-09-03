37d10be4a92c3f5e03d3b6156caafd4720ecddea
Ha Tran <nganha1897@gmail.com>
2024-09-03 03:06:13 -0400

    Time: 0 ms (100.00%) | Memory: 41.7 MB (17.97%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -13,16 +13,6 @@ class Solution {
         // }
         // return true;
 
-        if (s.length() == 1) {
-            if (s.charAt(0) == '1') {
-                return true;
-            }
-        }
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == '0' && s.charAt(i+1) == '1') {
-                return false;
-            }
-        }
-        return true;
+        return !s.contains("01");
     }
 }
