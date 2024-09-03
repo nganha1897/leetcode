e8f730f174004835762c34ee3c6ea4771f4bc64e
Ha Tran <nganha1897@gmail.com>
2024-09-03 01:52:03 -0400

    Time: 5 ms (13.97%) | Memory: 55.7 MB (94.78%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -8,15 +8,11 @@ class Solution {
         int sum = 0;
         for (int i=0, count=0; i<n && count<2*n; count++) {
             sum += gas[i];
-            if (sum < 0) {
-                i = (i + 1) % n; 
-                if (i == 0) {
-                    return -1;
-                }
+            i = (i + 1) % n; 
+            if (sum < 0) {         
                 ans = i;
                 sum = 0;
             } else {
-                i = (i + 1) % n; 
                 if (i == ans) {
                     return ans;
                 }
