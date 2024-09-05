f09ebee76e5a4b05355ae81fa776deb4985ea4e7
Ha Tran <nganha1897@gmail.com>
2024-09-05 18:21:38 -0400

    Time: 3 ms (98.14%) | Memory: 60.2 MB (59.63%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.java
+++ 2155-find-missing-observations/find-missing-observations.java
@@ -22,25 +22,24 @@
 // }
 public class Solution {
 
-    public int[] missingRolls(int[] rolls, int mean, int n) {
+     public int[] missingRolls(int[] rolls, int mean, int n) {
+        int[] ret = new int[n];
         int sum = 0;
-        for (int roll : rolls) {
-            sum = sum + roll;
+        for (int i : rolls) {
+            sum += i;
         }
-        // Find the remaining sum.
-        int remainingSum = mean * (n + rolls.length) - sum;
-        // Check if sum is valid or not.
-        if (remainingSum > 6 * n || remainingSum < n) {
-            return new int[0];
+        int missingSum = mean * (n + rolls.length) - sum;
+        if (missingSum > 6*n || missingSum < n) {
+            return new int[]{};
         }
-        int distributeMean = remainingSum / n;
-        int mod = remainingSum % n;
-        // Distribute the remaining mod elements in nElements array.
-        int[] nElements = new int[n];
-        Arrays.fill(nElements, distributeMean);
-        for (int i = 0; i < mod; i++) {
-            nElements[i]++;
+        int expectedAvg = missingSum/n, remainder = missingSum%n;
+
+        for (int i = 0; i < remainder; i++) {
+            ret[i] = expectedAvg + 1;
         }
-        return nElements;
+        for (int i = remainder; i < n;i++) {
+            ret[i] = expectedAvg;
+        }
+        return ret;
     }
 }
