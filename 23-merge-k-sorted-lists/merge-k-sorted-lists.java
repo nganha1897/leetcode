4f29b9dbff32867a270d946baaa54fd5ca103e39
Ha Tran <nganha1897@gmail.com>
2024-09-05 00:12:15 -0400

    Time: 5 ms (40.32%) | Memory: 45.5 MB (5.44%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,21 +9,43 @@
  * }
  */
 class Solution {
+    //public ListNode mergeKLists(ListNode[] lists) {
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // for (int i=0; i<lists.length; i++) {
+        //     ListNode cur = lists[i];
+        //     while (cur != null) {
+        //         pq.add(cur.val);
+        //         cur = cur.next;
+        //     }
+        // }
+        // ListNode dummy = new ListNode();
+        // ListNode cur = dummy;
+        // while (!pq.isEmpty()) {
+        //     int val = pq.poll();
+        //     cur.next = new ListNode(val);
+        //     cur = cur.next;
+        // }
+        // return dummy.next;
+    //}
+
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int offset = 10000;
+        int[] nums = new int[2 * offset + 1];
         for (int i=0; i<lists.length; i++) {
             ListNode cur = lists[i];
             while (cur != null) {
-                pq.add(cur.val);
+                nums[cur.val + offset]++;
                 cur = cur.next;
             }
         }
         ListNode dummy = new ListNode();
         ListNode cur = dummy;
-        while (!pq.isEmpty()) {
-            int val = pq.poll();
-            cur.next = new ListNode(val);
-            cur = cur.next;
+        for (int i=0; i<nums.length; i++) {
+            while (nums[i] > 0) {
+                cur.next = new ListNode(i - offset);
+                cur = cur.next;
+                nums[i]--;
+            }
         }
         return dummy.next;
     }
