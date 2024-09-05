3e43410ef22c9c853fb48e1a278b95f2ea79f0c5
Ha Tran <nganha1897@gmail.com>
2024-09-05 05:00:40 -0400

    Time: 3 ms (11.16%) | Memory: 45.1 MB (98.79%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,49 +1,60 @@
 class Solution {
+    // public int trap(int[] height) {
+    //     int n = height.length, ans = 0, maxLeft = 0, maxRight = 0;
+    //     for (int i = 1; i < n; i++) {
+    //         if (maxLeft[i - 1] == -1) {
+    //             if (height[i - 1] <= height[i])
+    //                 maxLeft[i] = -1;
+    //             else
+    //                 maxLeft[i] = i - 1;
+    //         } else {
+    //             if (height[maxLeft[i - 1]] <= height[i]) {
+    //                 maxLeft[i] = -1;
+    //             } else {
+    //                 maxLeft[i] = maxLeft[i - 1];
+    //             }
+    //         }
+    //     }
+
+    //     maxRight[n - 1] = -1;
+    //     for (int i = n-2; i>=0; i--) {
+    //         if (maxRight[i + 1] == -1) {
+    //             if (height[i + 1] <= height[i])
+    //                 maxRight[i] = -1;
+    //             else
+    //                 maxRight[i] = i + 1;
+    //         } else {
+    //             if (height[maxRight[i + 1]] <= height[i]) {
+    //                 maxRight[i] = -1;
+    //             } else {
+    //                 maxRight[i] = maxRight[i + 1];
+    //             }
+    //         }
+    //     }
+
+    //     for (int i = 0; i < n; i++) {
+    //         if (maxLeft[i] != -1 && maxRight[i] != -1) {
+    //             ans += Math.min(height[maxLeft[i]], height[maxRight[i]]) - height[i];
+    //         }
+    //     }
+
+    //     return ans;
+    // }
     public int trap(int[] height) {
-        int n = height.length, ans = 0;
-        int[] maxLeft = new int[n];
-        int[] maxRight = new int[n];
-
-        maxLeft[0] = -1;
-        for (int i = 1; i < n; i++) {
-            if (maxLeft[i - 1] == -1) {
-                if (height[i - 1] <= height[i])
-                    maxLeft[i] = -1;
-                else
-                    maxLeft[i] = i - 1;
-            } else {
-                if (height[maxLeft[i - 1]] <= height[i]) {
-                    maxLeft[i] = -1;
-                } else {
-                    maxLeft[i] = maxLeft[i - 1];
-                }
+        int ans = 0, current = 0;
+        Deque<Integer> st = new LinkedList<Integer>();
+        while (current < height.length) {
+            while (!st.isEmpty() && height[current] > height[st.peek()]) {
+                int top = st.peek();
+                st.pop();
+                if (st.isEmpty()) break;
+                int distance = current - st.peek() - 1;
+                int bounded_height =
+                    Math.min(height[current], height[st.peek()]) - height[top];
+                ans += distance * bounded_height;
             }
+            st.push(current++);
         }
-
-        maxRight[n - 1] = -1;
-        for (int i = n-2; i>=0; i--) {
-            if (maxRight[i + 1] == -1) {
-                if (height[i + 1] <= height[i])
-                    maxRight[i] = -1;
-                else
-                    maxRight[i] = i + 1;
-            } else {
-                if (height[maxRight[i + 1]] <= height[i]) {
-                    maxRight[i] = -1;
-                } else {
-                    maxRight[i] = maxRight[i + 1];
-                }
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (maxLeft[i] != -1 && maxRight[i] != -1) {
-                ans += Math.min(height[maxLeft[i]], height[maxRight[i]]) - height[i];
-            }
-        }
-
-        // System.out.println(Arrays.toString(maxLeft));
-        // System.out.println(Arrays.toString(maxRight));
         return ans;
     }
 }
