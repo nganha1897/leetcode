dbaa1c062c1209cd209a60e11866be4007ba2dcf
Ha Tran <nganha1897@gmail.com>
2024-09-06 19:17:49 -0400

    Time: 36 ms (18.86%) | Memory: 45.2 MB (21.45%) - LeetSync

--- 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.java
+++ 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.java
@@ -1,20 +1,11 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 class Solution {
     public int countValidWords(String sentence) {
         String[] w = sentence.split(" ");
         String regex = "^(([a-z]*)|([a-z]+-[a-z]+))[!.,]?$";
         int ans = 0;
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher;
+
         for (String s : w) {
-            if (s.length() == 0) {
-                continue;
-            }
-            matcher = pattern.matcher(s);
-            if (matcher.find()) {
-                System.out.println(s);
+            if (s.length() > 0 && s.matches(regex)) {
                 ans++;
             }
         }
