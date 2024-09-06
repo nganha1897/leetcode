678bcff5166e4f35b55964ffdad967821551b97c
Ha Tran <nganha1897@gmail.com>
2024-09-06 17:37:38 -0400

    Time: 5 ms (99.28%) | Memory: 71.8 MB (8.47%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -9,50 +9,51 @@
  * }
  */
 class Solution {
-    // public ListNode modifiedList(int[] nums, ListNode head) {
-    //     ListNode dummy = new ListNode(0, head);
-    //     ListNode cur = dummy;
-    //     int n = nums.length;
-    //     int[] rem = new int[100001];
-    //     for (int num : nums) {
-    //         rem[num]++;
-    //     }
-    //     while (cur.next != null) {
-    //         if (rem[cur.next.val] > 0) {
-    //             cur.next = cur.next.next;
-    //         } else
-    //             cur = cur.next;
-    //     }
-    //     return dummy.next;
-    // }
-    
     public ListNode modifiedList(int[] nums, ListNode head) {
-        int max = 0;
-        for(int n:nums){
-            if(n > max){
-                max = n;
-            }
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+        int n = nums.length;
+        boolean[] rem = new boolean[100001];
+        for (int num : nums) {
+            rem[num] = true;
         }
-        boolean[] seen = new boolean[max+1];
-
-        for(int n:nums){
-            seen[n] = true;
+        while (cur.next != null) {
+            if (rem[cur.next.val]) {
+                cur.next = cur.next.next;
+            } else
+                cur = cur.next;
         }
-        while(head != null && head.val <= max && seen[head.val]){
-            head = head.next;
-        }
-        if(head == null){
-            return head;
-        }
-        ListNode curr = head.next, prev = head;
-        while(curr != null){
-            if(curr.val <= max && seen[curr.val]){
-                prev.next = curr.next;
-            }else{
-                prev = curr;
-            }
-            curr = curr.next;
-        }        
-        return head;
+        return dummy.next;
     }
+    
+    // public ListNode modifiedList(int[] nums, ListNode head) {
+    //     int max = 0;
+    //     for(int n:nums){
+    //         if(n > max){
+    //             max = n;
+    //         }
+    //     }
+
+    //     boolean[] seen = new boolean[max+1];
+
+    //     for(int n:nums){
+    //         seen[n] = true;
+    //     }
+    //     while(head != null && head.val <= max && seen[head.val]){
+    //         head = head.next;
+    //     }
+    //     if(head == null){
+    //         return head;
+    //     }
+    //     ListNode curr = head.next, prev = head;
+    //     while(curr != null){
+    //         if(curr.val <= max && seen[curr.val]){
+    //             prev.next = curr.next;
+    //         }else{
+    //             prev = curr;
+    //         }
+    //         curr = curr.next;
+    //     }        
+    //     return head;
+    // }
 }
