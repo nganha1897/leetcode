46d5f5669f317844005c5c643281360c2cbfb680
Ha Tran <nganha1897@gmail.com>
2024-09-06 19:27:20 -0400

    Time: 3 ms (96.12%) | Memory: 44.4 MB (44.19%) - LeetSync

--- 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.java
+++ 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.java
@@ -1,14 +1,44 @@
 class Solution {
-    public int countValidWords(String sentence) {
-        String[] w = sentence.split(" ");
-        String regex = "^(([a-z]*)|([a-z]+-[a-z]+))[!.,]?$";
-        int ans = 0;
+    // public int countValidWords(String sentence) {
+    //     String[] w = sentence.split(" ");
+    //     String regex = "^(([a-z]*)|([a-z]+-[a-z]+))[!.,]?$";
+    //     int ans = 0;
 
+    //     for (String s : w) {
+    //         if (s.length() > 0 && s.matches(regex)) {
+    //             ans++;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    public int countValidWords(String sentence) {
+        int ans = 0;
+        String[] w = sentence.split(" ");
         for (String s : w) {
-            if (s.length() > 0 && s.matches(regex)) {
+            if (s.length() > 0 && isValid(s)) {
                 ans++;
             }
         }
         return ans;
+    }
+    private boolean isValid(String s) {
+        int hyphen = 0;
+        char[] a = s.toCharArray();
+
+        for (int i=0; i<a.length; i++) {
+            char c = a[i];
+            if ((c < 'a' || c > 'z') && (c != '-' && c != '!' && c != '.' && c != ',')) {
+                return false;
+            }
+            if (c == '-' && (hyphen > 0 || i == 0 || i == a.length-1 || a[i-1] < 'a' || a[i-1] > 'z' || a[i+1] < 'a' || a[i+1] > 'z')) {
+                return false;
+            } else if (c == '-') {
+                hyphen++;
+            }
+            if ((c == '!' || c == '.' || c == ',') && i < a.length-1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
