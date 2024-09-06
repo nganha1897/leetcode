cddaea0dd28999d78fbec027e461f2d43e7d8cf6
Ha Tran <nganha1897@gmail.com>
2024-09-06 17:31:43 -0400

    Time: 3 ms (100.00%) | Memory: 63.7 MB (97.93%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -9,20 +9,50 @@
  * }
  */
 class Solution {
+    // public ListNode modifiedList(int[] nums, ListNode head) {
+    //     ListNode dummy = new ListNode(0, head);
+    //     ListNode cur = dummy;
+    //     int n = nums.length;
+    //     int[] rem = new int[100001];
+    //     for (int num : nums) {
+    //         rem[num]++;
+    //     }
+    //     while (cur.next != null) {
+    //         if (rem[cur.next.val] > 0) {
+    //             cur.next = cur.next.next;
+    //         } else
+    //             cur = cur.next;
+    //     }
+    //     return dummy.next;
+    // }
+    
     public ListNode modifiedList(int[] nums, ListNode head) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode cur = dummy;
-        int n = nums.length;
-        int[] rem = new int[100001];
-        for (int num : nums) {
-            rem[num]++;
+        int max = 0;
+        for(int n:nums){
+            if(n > max){
+                max = n;
+            }
         }
-        while (cur.next != null) {
-            if (rem[cur.next.val] > 0) {
-                cur.next = cur.next.next;
-            } else
-                cur = cur.next;
+        boolean[] seen = new boolean[max+1];
+
+        for(int n:nums){
+            seen[n] = true;
         }
-        return dummy.next;
+        while(head != null && head.val <= max && seen[head.val]){
+            head = head.next;
+        }
+        if(head == null){
+            return head;
+        }
+        ListNode curr = head.next, prev = head;
+        while(curr != null){
+            if(curr.val <= max && seen[curr.val]){
+                prev.next = curr.next;
+            }else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }        
+        return head;
     }
 }
