40980ee1f881162f4b91b4abe30334c72436e977
Ha Tran <nganha1897@gmail.com>
2024-09-07 03:47:00 -0400

    Time: 1 ms (72.89%) | Memory: 41.2 MB (90.51%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,25 +1,37 @@
 class Solution {
+    // public String longestCommonPrefix(String[] strs) {
+    //     Trie root = new Trie();
+    //     for (String s : strs) {
+    //         root.add(s);
+    //     }
+    //     int end = -1, n = strs.length;
+    //     while (true) {
+    //         boolean hasPre = false;
+    //         for (int i=0; i<26; i++) {
+    //             if (root.children[i] != null && root.children[i].count == n) {
+    //                 root = root.children[i];
+    //                 hasPre = true;
+    //                 end++;
+    //                 break;
+    //             } 
+    //         }
+    //         if (!hasPre) {
+    //             break;
+    //         }
+    //     }
+    //     return end == -1 ? "" : strs[0].substring(0, end + 1);
+    // }
     public String longestCommonPrefix(String[] strs) {
-        Trie root = new Trie();
-        for (String s : strs) {
-            root.add(s);
-        }
-        int end = -1, n = strs.length;
-        while (true) {
-            boolean hasPre = false;
-            for (int i=0; i<26; i++) {
-                if (root.children[i] != null && root.children[i].count == n) {
-                    root = root.children[i];
-                    hasPre = true;
-                    end++;
-                    break;
-                } 
-            }
-            if (!hasPre) {
-                break;
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (
+                    i == strs[j].length() || strs[j].charAt(i) != c
+                ) return strs[0].substring(0, i);
             }
         }
-        return end == -1 ? "" : strs[0].substring(0, end + 1);
+        return strs[0];
     }
 }
 
