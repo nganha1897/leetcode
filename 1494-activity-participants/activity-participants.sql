baf8cec0ce1df59d7a8ceb5f370777dfefc18b24
Ha Tran <nganha1897@gmail.com>
2024-09-07 03:26:31 -0400

    Time: 591 ms (92.12%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -11,14 +11,28 @@
 -- and cnt != (select min(cnt) from user_activity)
 -- ;
 
-select activity 
-from
+-- select activity 
+-- from
+-- (
+-- select activity,
+-- dense_rank() over (order by count(id)) as rnk_asc,
+-- dense_rank() over (order by count(id) desc) as rnk_dec
+-- from friends
+-- group by activity
+-- ) user_activity
+-- where rnk_asc != 1 and rnk_dec != 1
+-- ;
+
+with user_activity as 
 (
-select activity,
-dense_rank() over (order by count(id)) as rnk_asc,
-dense_rank() over (order by count(id) desc) as rnk_dec
-from friends
+select activity, count(distinct id) cnt from friends
 group by activity
-) user_activity
-where rnk_asc != 1 and rnk_dec != 1
-;
+)
+select activity
+from 
+user_activity ua left join 
+  (
+    select max(cnt) cnt from user_activity union select min(cnt) cnt from user_activity
+  ) max_min
+on ua.cnt = max_min.cnt
+where max_min.cnt is null;
