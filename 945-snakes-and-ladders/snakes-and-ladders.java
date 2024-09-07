bfab4e0ad23fe71ce06fc41e3a727c81724e23d9
Ha Tran <nganha1897@gmail.com>
2024-09-07 00:28:44 -0400

    Time: 5 ms (81.16%) | Memory: 43.9 MB (99.33%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.java
+++ 945-snakes-and-ladders/snakes-and-ladders.java
@@ -1,7 +1,7 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
-        int n = board.length, dest = n*n;
-        int[] loc = new int[dest+1];
+        int n = board.length;
+        int[] loc = new int[n*n+1];
         
         int val = 1;
         boolean forDir = true;
@@ -18,27 +18,24 @@ class Solution {
             forDir = !forDir;
         }
 
-        boolean[] visited = new boolean[dest+1];
-        int start = (loc[1] == -1 ? 1 : loc[1]);
-        Deque<int[]> d = new ArrayDeque<>();
-        d.offerLast(new int[] {start, 0});
-        visited[start] = true;
-
-        while (!d.isEmpty()) {
-            int[] cur = d.pollFirst();
-            if (cur[0] == dest)
-                return cur[1];
-            for (int i=1; i<=6; i++) {
-                if (cur[0] + i > dest)
-                    break;
-                int next = loc[cur[0] + i] == -1 ? cur[0] + i : loc[cur[0] + i];
-                if (!visited[next]) {
-                    d.offerLast(new int[] {next, cur[1] + 1});
-                    visited[next] = true;
+        int[] dist = new int[n*n+1];
+        Arrays.fill(dist, n*n+1);
+        Deque<Integer> q = new LinkedList<>();
+        dist[1] = 0;
+        q.add(1);
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (cur == n*n) {
+                return dist[cur];
+            }
+            for (int next=cur+1; next<=Math.min(cur+6, n*n); next++) {                
+                int dest = loc[next] == -1 ? next : loc[next];
+                if (dist[dest] == n*n+1) {
+                    dist[dest] = dist[cur] + 1;
+                    q.add(dest);
                 }
             }
         }
-
-        return -1;
+        return dist[n*n] == n*n+1 ? -1 : dist[n*n];
     }
 }
