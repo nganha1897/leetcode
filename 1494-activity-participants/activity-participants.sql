2b9077fd56a75972e8bce1f04cb497f7b9e6e4a6
Ha Tran <nganha1897@gmail.com>
2024-09-07 03:10:55 -0400

    Time: 627 ms (78.74%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -1,21 +1,15 @@
 # Write your MySQL query statement below
 with max_cnt as 
-(select max(cnt) max_count
+(select max(cnt) max_count, min(cnt) min_count
 from (
 select count(*) cnt from friends
 group by activity
-) t2),
-min_cnt as
-(select min(cnt) min_count
-from (
-select count(*) cnt from friends
-group by activity
-) t3)
+) t2)
 select activity 
 from 
 (
 select activity, count(*) as act_cnt from friends 
 group by activity 
 having act_cnt != (select max_count from max_cnt)
-and act_cnt != (select min_count from min_cnt)
+and act_cnt != (select min_count from max_cnt)
 ) t1;
