a687c9b9e21b363908b535b6b89882a3dd7b4853
Ha Tran <nganha1897@gmail.com>
2024-09-06 23:09:59 -0400

    Time: 4 ms (28.68%) | Memory: 41.4 MB (96.81%) - LeetSync

--- 827-expressive-words/expressive-words.java
+++ 827-expressive-words/expressive-words.java
@@ -1,64 +1,102 @@
 class Solution {
+    // public int expressiveWords(String s, String[] words) {
+    // int ans = 0;
+    // char[] sarr = s.toCharArray();
+    // for (String w : words) {
+    // char[] warr = w.toCharArray();
+    // if (sarr[0] != warr[0]) {
+    // continue;
+    // }
+
+    // boolean isStretchy = true;
+    // int i=1, j=1, count = 1;
+    // while (i<sarr.length && j<warr.length) {
+    // if (sarr[i] == warr[j]) {
+    // if (sarr[i] == sarr[i-1]) {
+    // count++;
+    // } else {
+    // count = 1;
+    // }
+    // i++;
+    // j++;
+    // } else {
+    // if (sarr[i] != sarr[i-1]) {
+    // isStretchy = false;
+    // break;
+    // }
+    // while (i < sarr.length && sarr[i] == sarr[i-1]) {
+    // count++;
+    // i++;
+    // }
+    // if (count < 3) {
+    // isStretchy = false;
+    // break;
+    // }
+    // }
+    // }
+    // if (!isStretchy) {
+    // continue;
+    // }
+    // if (i == sarr.length && j == warr.length) {
+    // if (isStretchy) {
+    // ans++;
+    // }
+    // continue;
+    // }
+    // while (i < sarr.length) {
+    // if (sarr[i] != sarr[i-1]) {
+    // isStretchy = false;
+    // break;
+    // }
+    // i++;
+    // count++;
+    // }
+    // if (j < warr.length) {
+    // isStretchy = false;
+    // continue;
+    // }
+    // if (isStretchy && count >= 3) {
+    // ans++;
+    // }
+    // }
+    // return ans;
+    // }
+
     public int expressiveWords(String s, String[] words) {
         int ans = 0;
-        char[] sarr = s.toCharArray();
         for (String w : words) {
-            char[] warr = w.toCharArray();
-            if (sarr[0] != warr[0]) {
-                continue;
-            }
-            
+            int i = 0, j = 0;
             boolean isStretchy = true;
-            int i=1, j=1, count = 1;
-            while (i<sarr.length && j<warr.length) {
-                if (sarr[i] == warr[j]) {
-                    if (sarr[i] == sarr[i-1]) {
-                        count++;
-                    } else {
-                        count = 1;
-                    }
-                    i++;
-                    j++;
-                } else {
-                    if (sarr[i] != sarr[i-1]) {
-                        isStretchy = false; 
-                        break;
-                    } 
-                    while (i < sarr.length && sarr[i] == sarr[i-1]) {
-                        count++;
-                        i++;
-                    }
-                    if (count < 3) {
+            while (i < s.length() && j < w.length()) {
+                if (s.charAt(i) == w.charAt(j)) {
+                    int lenS = noOfRepetitions(s, i);
+                    int lenW = noOfRepetitions(w, j);
+                    if (lenS < lenW || (lenS > lenW && lenS < 3)) {
                         isStretchy = false;
                         break;
                     }
-                }
-            }
-            if (!isStretchy) {
-                continue;
-            }
-            if (i == sarr.length && j == warr.length) {
-                if (isStretchy) {
-                    ans++;
-                }
-                continue;
-            }
-            while (i < sarr.length) {
-                if (sarr[i] != sarr[i-1]) {
+                    i += lenS;
+                    j += lenW;
+                } else {
                     isStretchy = false;
                     break;
                 }
-                i++;
-                count++;
             }
-            if (j < warr.length) {
-                isStretchy = false;
+            if (i < s.length() || j < w.length()) {
                 continue;
             }
-            if (isStretchy && count >= 3) {
+            if (isStretchy)
                 ans++;
-            }
         }
         return ans;
+    }
+
+    private int noOfRepetitions(String s, int i) {
+        for (int j = i + 1; j < s.length(); j++) {
+            if (s.charAt(j) != s.charAt(i)) {
+                return j - i;
+            }
+        }
+        return s.length() - i;
     }
 }
