0adb992ed19273c328ce15e0488cc64beab23c38
Ha Tran <nganha1897@gmail.com>
2024-09-07 03:21:52 -0400

    Time: 690 ms (55.71%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -1,12 +1,24 @@
 # Write your MySQL query statement below
-with user_activity as 
-(
-select activity, count(distinct id) cnt from friends
-group by activity
-)
+-- with user_activity as 
+-- (
+-- select activity, count(distinct id) cnt from friends
+-- group by activity
+-- )
+-- select activity 
+-- from 
+-- user_activity
+-- where cnt != (select max(cnt) from user_activity)
+-- and cnt != (select min(cnt) from user_activity)
+-- ;
+
 select activity 
-from 
-user_activity
-where cnt != (select max(cnt) from user_activity)
-and cnt != (select min(cnt) from user_activity)
+from
+(
+select activity,
+dense_rank() over (order by count(id)) as rnk_asc,
+dense_rank() over (order by count(id) desc) as rnk_dec
+from friends
+group by activity
+) user_activity
+where rnk_asc != 1 and rnk_dec != 1
 ;
