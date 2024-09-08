52938275a6dcde7c11132d02cdac2ee52df251b7
Ha Tran <nganha1897@gmail.com>
2024-09-08 03:46:44 -0400

    Time: 1 ms (100.00%) | Memory: 57.1 MB (18.55%) - LeetSync

--- 3374-count-alternating-subarrays/count-alternating-subarrays.java
+++ 3374-count-alternating-subarrays/count-alternating-subarrays.java
@@ -2,7 +2,7 @@ class Solution {
     public long countAlternatingSubarrays(int[] nums) {
         long ans = 1, sum = 1;
         for (int i=1; i<nums.length; i++) {
-            sum = (nums[i] == nums[i-1] ? 0 : sum) + 1;
+            sum = (nums[i] != nums[i-1] ? sum : 0) + 1;
             ans += sum;
         }
         return ans;
