867221fe51b4b1d9b25a87bef9ff25814cad72a9
Ha Tran <nganha1897@gmail.com>
2024-09-07 23:49:27 -0400

    Time: 1 ms (100.00%) | Memory: 41.9 MB (30.80%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,14 +1,25 @@
 class Solution {
+    // public char findTheDifference(String s, String t) {
+    //     int[] f = new int[26];
+    //     for (char c : s.toCharArray()) {
+    //         f[c-'a']++;
+    //     }
+    //     for (char c : t.toCharArray()) {
+    //         if (--f[c-'a'] < 0) {
+    //             return c;
+    //         }
+    //     }
+    //     return 'a';
+    // }
+
     public char findTheDifference(String s, String t) {
-        int[] f = new int[26];
-        for (char c : s.toCharArray()) {
-            f[c-'a']++;
-        }
+        int sum = 0;
         for (char c : t.toCharArray()) {
-            if (--f[c-'a'] < 0) {
-                return c;
-            }
+            sum += c;
         }
-        return 'a';
+        for (char c : s.toCharArray()) {
+            sum -= c;
+        }
+        return (char)sum;
     }
 }
