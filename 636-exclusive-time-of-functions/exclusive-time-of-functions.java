c2100068d569e750ed84aff5dec03c64dd5d0f9d
Ha Tran <nganha1897@gmail.com>
2024-09-09 00:02:29 -0400

    Time: 13 ms (23.73%) | Memory: 45.6 MB (10.07%) - LeetSync

---
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] ans = new int[n];
+        Deque<int[]> d = new ArrayDeque<>();
+        for (int i=0; i<logs.size(); i++) {
+            String[] cur = logs.get(i).split(":");
+            int id = Integer.valueOf(cur[0]);
+            int time = Integer.valueOf(cur[2]);
+            if (cur[1].equals("start")) {
+                if (!d.isEmpty()) {
+                    int[] prev = d.peekLast();
+                    ans[prev[0]] += time - prev[1];
+                }
+                d.offerLast(new int[] {id, time});
+            } else {
+                int[] start = d.pollLast();
+                ans[start[0]] += time - start[1] + 1;
+                if (!d.isEmpty()) {
+                    int[] prev = d.pollLast();
+                    //ans[prev[0]] += start[1] - prev[1];
+                    d.offerLast(new int[] {prev[0], time+1});
+                }
+            }
+        }
+        return ans;
+    }
+}
