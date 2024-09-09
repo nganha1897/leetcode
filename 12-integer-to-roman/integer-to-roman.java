20c058746660a8ca590c80256f6d7bf1c8595d7f
Ha Tran <nganha1897@gmail.com>
2024-09-09 02:09:54 -0400

    Time: 4 ms (58.97%) | Memory: 44.8 MB (22.25%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,33 +1,32 @@
 class Solution {
     public String intToRoman(int num) {
         StringBuilder sb = new StringBuilder();
-        Map<Integer, Character> map = new HashMap<>();
-        map.put(1, 'I');
-        map.put(5, 'V');
-        map.put(10, 'X');
-        map.put(50, 'L');
-        map.put(100, 'C');
-        map.put(500, 'D');
-        map.put(1000, 'M');
-
+        char[] symbols = new char[1001];
+        symbols[1] = 'I';
+        symbols[5] = 'V';
+        symbols[10] = 'X';
+        symbols[50] = 'L';
+        symbols[100] = 'C';
+        symbols[500] = 'D';
+        symbols[1000] = 'M';
         int mult = 1;
         while (num > 0) {
             int cur = num % 10;
             if (cur == 4) {
-                sb.append(map.get(mult * 5)).append(map.get(mult));
+                sb.append(symbols[mult * 5]).append(symbols[mult]);
             } else if (cur == 9) {
-                sb.append(map.get(mult * 10)).append(map.get(mult));
+                sb.append(symbols[mult * 10]).append(symbols[mult]);
             } else {
                 while (cur > 5) {
-                    sb.append(map.get(mult));
+                    sb.append(symbols[mult]);
                     cur--;
                 }
                 if (cur == 5) {
-                    sb.append(map.get(mult * 5));
+                    sb.append(symbols[mult * 5]);
                     cur -= 5;
                 }
                 while (cur > 0) {
-                    sb.append(map.get(mult));
+                    sb.append(symbols[mult]);
                     cur--;
                 }
             }
