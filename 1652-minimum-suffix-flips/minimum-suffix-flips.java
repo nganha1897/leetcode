25e741e677b84229867bbe02b7e68b352023e452
Ha Tran <nganha1897@gmail.com>
2024-09-08 22:34:19 -0400

    Time: 7 ms (42.93%) | Memory: 44.7 MB (43.47%) - LeetSync

--- 1652-minimum-suffix-flips/minimum-suffix-flips.java
+++ 1652-minimum-suffix-flips/minimum-suffix-flips.java
@@ -2,12 +2,9 @@ class Solution {
     public int minFlips(String target) {
         int ans = 0;
         for (int i=0; i<target.length(); i++) {
-            char c = target.charAt(i);
-            if (c == '1' && (ans % 2) == 0) {
+            int c = target.charAt(i) - 0;
+            if ((c + ans) % 2 == 1)
                 ans++;
-            } else if (c == '0' && (ans % 2) == 1) {
-                ans++;
-            }
         }
         return ans;
     }
