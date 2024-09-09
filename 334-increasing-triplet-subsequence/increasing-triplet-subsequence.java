222a78b4a7dc0f17de1b50902bf49e0b3cac78ba
Ha Tran <nganha1897@gmail.com>
2024-09-09 04:53:12 -0400

    Time: 6 ms (7.41%) | Memory: 134 MB (37.22%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        Integer i = null, j = null;
+        for (int k = 0; k<nums.length; k++) {
+            if (j == null) {
+                j = nums[k];
+            } else if (i == null) {
+                if (nums[k] > j) {
+                    i = j;
+                    j = nums[k];
+                } else if (nums[k] < j) {
+                    j = nums[k];
+                }
+            } else {
+                if (nums[k] > j) {
+                    return true;
+                } else if (nums[k] < j) {
+                    if (nums[k] < i) {
+                        i = nums[k];
+                    } else if (nums[k] > i) {
+                        j = nums[k];
+                    }
+                }
+            }
+            //System.out.println(i + " " + j + " " + nums[k]);
+        }
+        return false;
+    }
+}
