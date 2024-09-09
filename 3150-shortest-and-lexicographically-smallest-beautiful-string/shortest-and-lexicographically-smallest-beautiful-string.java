66ad227b8a9c9cb69ca6dc2a5376f16e2738ecec
Ha Tran <nganha1897@gmail.com>
2024-09-08 22:05:10 -0400

    Time: 1 ms (100.00%) | Memory: 42.4 MB (83.67%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        String ans = "";
+        int start = 0, count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += s.charAt(i) - '0';
+            while (count > k || (count == k && s.charAt(start) == '0')) {
+                count -= s.charAt(start++) - '0';
+            }
+            if (count == k) {
+                String cur = s.substring(start, i + 1);
+                //System.out.println(cur);
+                if (ans.length() == 0 || ans.length() > cur.length()) {
+                    ans = cur;
+                } else if (ans.length() == cur.length()) {
+                    boolean isSmaller = isSmaller(cur, ans);
+                    //System.out.println(cur + " " + ans + " " + isSmaller);
+                    if (isSmaller) {
+                        ans = cur;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    private boolean isSmaller(String s, String t) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) > t.charAt(i)) {
+                //System.out.println(s + " " + t + " " + i);
+                return false;
+            } else if (s.charAt(i) < t.charAt(i)) {
+                return true;
+            }
+        }
+        return true;
+    }
+}
