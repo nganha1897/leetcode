f312ac40c690e69998881a8439159f1587147b68
Ha Tran <nganha1897@gmail.com>
2024-09-08 20:44:41 -0400

    Time: 0 ms (100.00%) | Memory: 40.1 MB (90.20%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,12 +1,20 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        for (int i=1, count=0; i<=n; i++) {
-            if (n % i == 0) {
-                if (++count == k) {
-                    return i;
-                }
-            }
-        }
+        // for (int i=1, count=0; i<=n; i++) {
+        // if (n % i == 0) {
+        // if (++count == k) {
+        // return i;
+        // }
+        // }
+        // }
+        // return -1;
+
+        for (int i = 1; i < Math.sqrt(n); ++i)
+            if (n % i == 0 && --k == 0)
+                return i;
+        for (int i = (int) Math.sqrt(n); i >= 1; --i)
+            if (n % (n / i) == 0 && --k == 0)
+                return n / i;
         return -1;
     }
 }
