222f6ffe3ffae079eb130d4c0f11aaa7e7057544
Ha Tran <nganha1897@gmail.com>
2024-09-09 00:42:51 -0400

    Time: 67 ms (18.31%) | Memory: 61.6 MB (18.78%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
@@ -3,15 +3,10 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             if (num < 0) {
-                //System.out.println(num);
-                //num = num + (-num / value + (-num % value == 0 ? 0 : 1)) * value;
-                //System.out.println(num);
                 num = num % value + value;
-                //System.out.println(-13 % 5);
             }
             map.put(num % value, map.getOrDefault(num % value, 0) + 1);
         }
-        //System.out.println(map);
 
         for (int i = 0; i < nums.length; i++) {
             int mod = i % value;
