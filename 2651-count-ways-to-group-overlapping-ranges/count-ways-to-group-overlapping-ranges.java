be1fbfa95aae4b6e2e613ecb5db0a18ba6da8444
Ha Tran <nganha1897@gmail.com>
2024-09-08 21:34:10 -0400

    Time: 19 ms (65.83%) | Memory: 81.8 MB (6.03%) - LeetSync

--- 2651-count-ways-to-group-overlapping-ranges/count-ways-to-group-overlapping-ranges.java
+++ 2651-count-ways-to-group-overlapping-ranges/count-ways-to-group-overlapping-ranges.java
@@ -1,33 +1,15 @@
 class Solution {
     public int countWays(int[][] ranges) {
-        List<int[]> groups  = new ArrayList<>();
         Arrays.sort(ranges, (a,b) -> a[0] - b[0]);
-
-        for (int i=0; i<ranges.length; i++) {
-            if (groups.size() == 0) {
-                groups.add(ranges[i]);
+        int mod = (int)1e9 + 7;
+        long ans = 2;
+        for (int i=1; i<ranges.length; i++) {
+            if (ranges[i][0] > ranges[i-1][1]) {
+                ans = (ans * 2) % mod;
             } else {
-                int last = groups.size() - 1;
-                if (groups.get(last)[1] >= ranges[i][0]) {
-                    groups.get(last)[1] = Math.max(groups.get(last)[1], ranges[i][1]);
-                } else {
-                    groups.add(ranges[i]);
-                }
+                ranges[i][1] = Math.max(ranges[i][1], ranges[i-1][1]);
             }
         }
-
-        return (int)(power(2, groups.size(), (int)(1e9+7)));
-    }
-    private long power(int x, int p, int mod) {
-        if (p == 0) {
-            return 1;
-        }
-
-        long res = power(x, p >> 1, mod);
-        res = (res * res) % mod;
-        if (p % 2 == 1) {
-            res = (res * x) % mod;
-        }
-        return res;
+        return (int)ans;
     }
 }
