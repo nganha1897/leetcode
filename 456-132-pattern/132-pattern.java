6cf5d59220433eabfa1007ddb0c936cff92882de
Ha Tran <nganha1897@gmail.com>
2024-09-10 00:20:04 -0400

    Time: 17 ms (83.51%) | Memory: 75 MB (71.69%) - LeetSync

--- 456-132-pattern/132-pattern.java
+++ 456-132-pattern/132-pattern.java
@@ -3,19 +3,20 @@ class Solution {
         int n = nums.length;
         int[] min = new int[n];
         min[0] = nums[0];
-        for (int i=1; i<n; i++) {
-            min[i] = Math.min(min[i-1], nums[i]);
+        for (int i = 1; i < n; i++) {
+            min[i] = Math.min(min[i - 1], nums[i]);
         }
         Deque<Integer> d = new ArrayDeque<>();
-        for (int i=n-1; i>=0; i--) {
-            while (!d.isEmpty() && nums[d.peekLast()] <= min[i]) {
-                d.pollLast();
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] > min[i]) {
+                while (!d.isEmpty() && nums[d.peekLast()] <= min[i]) {
+                    d.pollLast();
+                }
+                if (!d.isEmpty() && nums[d.peekLast()] < nums[i] && nums[i] > min[i] && nums[d.peekLast()] > min[i]) {
+                    return true;
+                }
+                d.offerLast(i);
             }
-            if (!d.isEmpty() && nums[d.peekLast()] < nums[i] && nums[i] > min[i] && nums[d.peekLast()] > min[i]) {
-                //System.out.println(nums[d.peekLast()] + " " + nums[i] + " " + min[i]);
-                return true;
-            }
-            d.offerLast(i);
         }
         return false;
     }
