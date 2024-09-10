535e5cfe151b58d1c7a989d4b21c33709b7ae0f5
Ha Tran <nganha1897@gmail.com>
2024-09-10 16:10:01 -0400

    Time: 70 ms (29.95%) | Memory: 45.1 MB (25.26%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -3,17 +3,23 @@ class Solution {
         int sum = 0;
         for (int num : nums) {
             int div = 0;
+            Set<Integer> n = new HashSet<>();
             for (int i=1; i<=Math.sqrt(num); i++) {
                 if (num % i == 0) {
                     div += i * i == num ? 1 : 2;
+                    n.add(i);
+                    n.add(num / i);
                 }
             }
             if (div == 4) {
-                for (int i=1; i<Math.sqrt(num); i++) {
-                    if (num % i == 0 && num / i != i) {
-                        sum += i + num / i;
-                    }
+                for (int i : n) {
+                    sum += i;
                 }
+                // for (int i=1; i<Math.sqrt(num); i++) {
+                //     if (num % i == 0 && num / i != i) {
+                //         sum += i + num / i;
+                //     }
+                // }
             }
         }
         return sum;
