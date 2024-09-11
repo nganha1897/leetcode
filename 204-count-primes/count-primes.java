f9687e3d7dba404868000d81a1bf6ee10774b5ec
Ha Tran <nganha1897@gmail.com>
2024-09-11 13:43:11 -0400

    Time: 106 ms (40.57%) | Memory: 48.9 MB (86.19%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -7,18 +7,15 @@ class Solution {
         boolean[] primes = new boolean[n];
         Arrays.fill(primes, true);
         primes[2] = true;
-        for (int i=2; i*i<n; i++) {
-            if (primes[i]) {
-                for (int j=i*i;j<n; j+=i) {
-                    primes[j] = false;
-                }
-            }
-        }
         for (int i=2; i<n; i++) {
             if (primes[i]) {
                 ans++;
+                for (long j=(long)i*i;j<n; j+=i) {
+                    primes[(int)j] = false;
+                }
             }
         }
+
         return ans;
     }
 }
