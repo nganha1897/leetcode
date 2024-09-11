748a4e0e067dc26ff418706228506fd9d85fbf8d
Ha Tran <nganha1897@gmail.com>
2024-09-11 13:47:13 -0400

    Time: 71 ms (97.58%) | Memory: 48.9 MB (86.19%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,25 +1,51 @@
 class Solution {
-    public int countPrimes(int n) {
-        if (n <= 2) {
+//     public int countPrimes(int n) {
+//         if (n <= 2) {
+//             return 0;
+//         }
+//         int ans = 0;
+//         boolean[] primes = new boolean[n];
+//         Arrays.fill(primes, true);
+//         primes[2] = true;
+//         for (int i = 2; i < n; i++) {
+//             if (primes[i]) {
+//                 ans++;
+//                 //System.out.println(i);
+//                 if (i <= n / i) {
+//                     for (int j = i * i; j < n; j += i) {
+//                         //System.out.println(i + " " + j);
+//                         primes[j] = false;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+
+public int countPrimes(int n) {
+        if(n<=2){
             return 0;
         }
-        int ans = 0;
-        boolean[] primes = new boolean[n];
-        Arrays.fill(primes, true);
-        primes[2] = true;
-        for (int i = 2; i < n; i++) {
-            if (primes[i]) {
-                ans++;
-                //System.out.println(i);
-                if (i <= n / i) {
-                    for (int j = i * i; j < n; j += i) {
-                        //System.out.println(i + " " + j);
-                        primes[j] = false;
-                    }
+        boolean[] isPrime = new boolean[n];
+        for (int i = 3; i < n; i += 2) {
+            isPrime[i] = true;
+        }
+        isPrime[2] = true; 
+        for (int i=3; i*i<n; i+=2) {
+            if (isPrime[i]) {
+                for (int j = i*i; j<n; j+=2*i) {
+                    isPrime[j] = false;
                 }
             }
         }
+        int count = 0;
+        for (int i=2; i<n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
 
-        return ans;
+        return count;
     }
 }
