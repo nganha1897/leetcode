7ea522c05e6b649e22a27c11d9ab23dc3d99fb67
Ha Tran <nganha1897@gmail.com>
2024-09-16 03:13:11 -0400

    Time: 42 ms (25.76%) | Memory: 43 MB (68.18%) - LeetSync

---
+++ 2209-number-of-equal-count-substrings/number-of-equal-count-substrings.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int equalCountSubstrings(String s, int count) {
+        int ans = 0;
+        int n = s.length();
+        
+        for (int i=1; i<=26; i++) {
+            int len = i * count;
+            if (len > n) {
+                return ans;
+            }
+            int[] dp = new int[26];
+            int unique = 0;
+            for (int st=0, j=0; j<n; j++) {
+                int c = s.charAt(j) - 'a';
+                if (dp[c]++ == 0) {
+                    unique++;
+                }
+                if (j > len - 1) {
+                    if (--dp[s.charAt(st++)-'a'] == 0) {
+                        unique--;
+                    }
+                }
+                if (j>= len-1) {
+                    if (unique == i) {
+                        boolean isEqCnt = true;
+                        for (int k=0; k<26; k++) {
+                            if (dp[k] > 0 && dp[k] < count) {
+                                isEqCnt = false;
+                                break;
+                            }
+                        }
+                        if (isEqCnt)
+                            ans++;
+                        //System.out.println(st + " " + j + " " + count + " " + len + " " + ans);
+                    }
+                }
+                //System.out.println(i + " " + st + " " + j + " " + unique);
+            }
+        }
+        return ans;
+    }
+}
