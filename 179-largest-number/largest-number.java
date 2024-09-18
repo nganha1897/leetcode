cb75b86f5c6d97b35183ba0a25c818675463eb84
Ha Tran <nganha1897@gmail.com>
2024-09-18 19:34:59 -0400

    Time: 10 ms (15.57%) | Memory: 44.9 MB (5.51%) - LeetSync

---
+++ 179-largest-number/largest-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        StringBuilder ans = new StringBuilder();
+        PriorityQueue<StringBuilder> pq = new PriorityQueue<>((a, b) -> isFront(a, b));
+        for (int n : nums) {
+            pq.add(new StringBuilder(n + ""));
+        }
+        while (!pq.isEmpty()) {
+            ans.append(pq.poll());
+        }
+        while (ans.length() > 1 && ans.charAt(0) == '0') {
+            ans.deleteCharAt(0);
+        } 
+        return ans.toString();
+    }
+
+    private int isFront(StringBuilder a, StringBuilder b) {
+        StringBuilder front = new StringBuilder(a + "" + b);
+        StringBuilder back = new StringBuilder(b + "" + a);
+        for (int i=0; i<front.length(); i++) {
+            if (front.charAt(i) < back.charAt(i)) {
+                return 1;
+            } else if (front.charAt(i) > back.charAt(i)) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+}
