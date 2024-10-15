bb4318ddf3367ee0f533273be992702ac8c44508
Sarah Tran <nganha1897@gmail.com>
2024-10-15 15:25:44 -0400

    Time: 30 ms (76.68%) | Memory: 59.1 MB (49.72%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Deque<Integer> decreasingStack = new ArrayDeque<>();
         int n = nums.length, pos = 0;
         int[] ans = new int[n-k+1];
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-
-            if (i >= k) {
-                map.put(nums[i - k], map.get(nums[i - k]) - 1);
-                if (map.get(nums[i - k]) == 0) {
-                    map.remove(nums[i - k]);
-                }
+        for (int i=0; i<n; i++) {
+            while (!decreasingStack.isEmpty() && nums[decreasingStack.peekLast()] <= nums[i]) {
+                decreasingStack.pollLast();
             }
-            if (i >= k - 1) {
-                ans[pos++] = map.lastKey();
+            while (!decreasingStack.isEmpty() && decreasingStack.peekFirst() < i - k + 1) {
+                decreasingStack.pollFirst();
             }
+            decreasingStack.offerLast(i);
+            if (i >= k - 1)
+                ans[pos++] = nums[decreasingStack.peekFirst()];
         }
         return ans;
     }
