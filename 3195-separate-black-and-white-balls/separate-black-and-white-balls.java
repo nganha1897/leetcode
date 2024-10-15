a2a081948cf752bf3d43be4e28d2c96a453228d4
Sarah Tran <nganha1897@gmail.com>
2024-10-15 14:23:52 -0400

    Time: 7 ms (100.00%) | Memory: 45.5 MB (41.94%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long ans = 0;
+        int black = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                ans += i - black;
+                black++;
+            }
+        }
+        return ans;
+    }
+}
