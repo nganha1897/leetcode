b6cd5c388e631b4deac1baf0ffcf35480ce6c1c7
Sarah Tran <nganha1897@gmail.com>
2025-04-05 18:53:10 -0400

    Time: 0 ms (100.00%) | Memory: 41.3 MB (30.29%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,32 +1,11 @@
-/*class Solution {
-    public int subsetXORSum(int[] nums) {
-        int[] subsetXOR = new int[(int)Math.pow(2,nums.length)];
-        subsetXORSum(nums, subsetXOR, 0, 1);
-        int sum=0;
-        for (int num : subsetXOR)
-            sum += num;
-        return sum;
-    }
-    private void subsetXORSum(int[] nums, int[] subsetXOR, int curr, int subsets) {
-        if (curr == nums.length)
-            return;
-        for (int i=0; i<subsets; i++) {
-            subsetXOR[subsets+i] = subsetXOR[i] ^ nums[curr];
-        }
-        subsetXORSum(nums, subsetXOR, curr+1, subsets*2);
-    }
-}*/
 class Solution {
-    //int sum=0;
     public int subsetXORSum(int[] nums) {
-        //sum=0;
-        return getAns(nums,0,0);
-    }
-    
-    int getAns(int[] arr,int i,int cur){
-        if(i==arr.length){
-            return cur;
+        int result = 0;
+        // Capture each bit that is set in any of the elements
+        for (int num : nums) {
+            result |= num;
         }
-        return getAns(arr,i+1,cur^arr[i]) + getAns(arr,i+1,cur);
+        // Multiply by the number of subset XOR totals that will have each bit set
+        return result << (nums.length - 1);
     }
 }
